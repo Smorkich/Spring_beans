f42c777783927203c8f42db15e61f8808b3e9833
руслан каракян <ruslan.karakyan@yandex.ru>
2023-11-24 14:07:27 +0300

    проходит тест

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,7 +1,14 @@
 package app.model;
 
-public class Timer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Timer {
+    @Autowired
+    @Qualifier("animalsCage")
+    private AnimalsCage animalsCage;
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
